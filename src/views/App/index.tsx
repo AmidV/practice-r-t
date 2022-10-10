1009812a506d7f19aca18d070e765b0f2b4e72e7
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-10-10 16:56:33 +0100

    add some product_s logic and btns response

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Product } from '../components/Product';
+
+import { products } from '../../data/stores/products';
+
 import styles from './index.module.scss';
+
 
 const App: React.FC = () => {
   return (
     <article className={styles.container}>
         <h1>Hello World!</h1>
+        <Product product={ products[0] }/>
+        <Product product={ products[1] }/>
     </article>
   )
 }
---
+++ src/views/components/Product.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { IProduct } from '../../models';
+import  styles from './Product.module.scss';
+
+interface ProductProps {
+    product: IProduct
+}
+
+export const Product = ({ product }: ProductProps) => {
+    const [details, setDetails] = useState(false); 
+  
+    function detailsHandler() {
+        return setDetails(!details)
+    }
+    return (
+    <div className={styles.card}>
+        <img src={product.image} className={styles.cardImg} alt={product.title}/>
+        <p>{product.title}</p>
+        <p className={styles.cardPrice}>{ product.price }</p>
+
+       {!details && <button className={styles.cardBtn} onClick={detailsHandler}>Show Details</button>}
+        {details && <p>{ product.description }</p>}
+        {details && <button className={styles.cardBtn2} onClick={detailsHandler}>less...</button>}
+    </div>
+  )
+}
